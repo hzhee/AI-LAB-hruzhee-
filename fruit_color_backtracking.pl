c36b7b24715185ded4a6f0d1b1699b8b5f6688d1
Hruzheecayshan T K <cayshanh@gmail.com>
2026-08-16 21:10:44 +0530

    Add Prolog fruit color backtracking experiment

---
+++ fruit_color_backtracking.pl
@@ -0,0 +1,11 @@
+fruit_color(apple, red).
+fruit_color(banana, yellow).
+fruit_color(grape, purple).
+fruit_color(orange, orange).
+
+list_fruits :-
+    fruit_color(Fruit, Color),
+    format('~w is ~w~n', [Fruit, Color]),
+    fail.
+
+list_fruits.
