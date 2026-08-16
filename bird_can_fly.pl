dcdbf92615265d62d7f01fbb54e40a953ddc8828
Hruzheecayshan T K <cayshanh@gmail.com>
2026-08-16 21:07:03 +0530

    Add Prolog bird can fly experiment

---
+++ bird_can_fly.pl
@@ -0,0 +1,13 @@
+can_fly(sparrow).
+can_fly(eagle).
+can_fly(parrot).
+cannot_fly(penguin).
+cannot_fly(ostrich).
+
+bird_can_fly(Bird) :-
+    can_fly(Bird),
+    format('~w can fly.~n', [Bird]).
+
+bird_can_fly(Bird) :-
+    cannot_fly(Bird),
+    format('~w cannot fly.~n', [Bird]).
