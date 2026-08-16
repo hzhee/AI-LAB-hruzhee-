ad55c1a39082d8ee7c470e0e28a730bfb40a2ef2
Hruzheecayshan T K <cayshanh@gmail.com>
2026-08-16 21:04:13 +0530

    Add Prolog plants database

---
+++ plants_database.pl
@@ -0,0 +1,8 @@
+plant(mercury, 58, rocky).
+plant(venus, 108, rocky).
+plant(earth, 150, rocky).
+plant(mars, 228, rocky).
+plant(jupiter, 778, 'gas-giant').
+plant(saturn, 1430, 'gas-giant').
+plant(uranus, 2870, 'ice-giant').
+plant(neptune, 4500, 'ice-giant').
